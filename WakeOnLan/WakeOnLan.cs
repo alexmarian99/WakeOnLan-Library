2866d03b382ed80084fc6459cc3b66ec0d71bc34
Alexandru Buza <alexandrumarianbuza@gmail.com>
2022-09-20 21:46:43 +0300

    Add wake on lan class

---
+++ WakeOnLan/WakeOnLan.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WakeOnLan
+{
+    public class WoLClient
+    {
+        /// <summary>
+        /// Send an WOL Packet to broadcast router.
+        /// </summary>
+        /// <param name="MAC_ADDRESS">Ex: AA:EE:CC:FF:DD</param>
+        public void WakeFunction(string MAC_ADDRESS)
+        {
+            WOLClass client = new WOLClass();
+            client.Connect(new
+               IPAddress(0xffffffff),  //255.255.255.255  i.e broadcast
+               0x2fff); // port=12287 let's use this one 
+            client.SetClientToBrodcastMode();
+            //set sending bites
+            int counter = 0;
+            //buffer to be send
+            byte[] bytes = new byte[1024];   // more than enough :-)
+                                             //first 6 bytes should be 0xFF
+            for (int y = 0; y < 6; y++)
+                bytes[counter++] = 0xFF;
+            //now repeate MAC 16 times
+            for (int y = 0; y < 16; y++)
+            {
+                int i = 0;
+                for (int z = 0; z < 6; z++)
+                {
+                    bytes[counter++] =
+                        byte.Parse(MAC_ADDRESS.Substring(i, 2), NumberStyles.HexNumber);
+                    i += 2;
+                }
+            }
+
+            //now send wake up packet
+            int reterned_value = client.Send(bytes, 1024);
+        }
+    }
+
+    internal class WOLClass : UdpClient
+    {
+        internal WOLClass() : base()
+        { }
+        //this is needed to send broadcast packet
+        internal void SetClientToBrodcastMode()
+        {
+            if (this.Active)
+                this.Client.SetSocketOption(SocketOptionLevel.Socket,
+                                          SocketOptionName.Broadcast, 0);
+        }
+    }
+
+}
